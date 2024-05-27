0dc23dcf5f2138abdae3a1459fdbd74857aeb2ea
Andrew <aks13@ukr.net>
2024-05-27 10:26:17 +0300

    Updated entities

--- src/product/entities/product.entity.ts
+++ src/product/entities/product.entity.ts
@@ -1,4 +1,4 @@
-import { Column, Entity, Index, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
 import { ProductRemnant } from '../../productRemnants/entities/productRemnant.entity';
 @Entity({
   name: 'products',
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,10 +1,15 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 import { Exclude } from 'class-transformer';
 
-@Entity()
+@Entity({
+  name: 'users',
+})
 export class User {
   @PrimaryGeneratedColumn()
   id: number;
+
+  @Column({ unique: true })
+  phone: string;
 
   @Column({ unique: true })
   email: string;
@@ -13,16 +18,19 @@ export class User {
   @Column()
   password: string;
 
-  @Column()
+  @Column({ name: 'first_name', length: 30 })
   firstName: string;
 
-  @Column()
+  @Column({ name: 'last_name', length: 30 })
   lastName: string;
 
-  @Column({ default: true })
+  @Column({ name: 'date_of_birth', length: 30 })
+  dateOfBirth: string;
+
+  @Column({ name: 'is_active', default: true })
   isActive: boolean;
 
-  @Column({ name: 'userType', default: 1 })
+  @Column({ name: 'user_type', default: 1 })
   userType: number;
 
   constructor(partial: Partial<User>) {
