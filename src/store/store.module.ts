65b70492d06842988483655c0fc0f53b05cd6091
Andrew <aks13@ukr.net>
2024-05-30 15:54:31 +0300

    Update stores

--- src/store/store.module.ts
+++ src/store/store.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Store } from './entities/store.entity';
 import { StoreController } from './store.controller';
 import { StoreService } from './store.service';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Store])],
+  imports: [TypeOrmModule.forFeature([Store]), CacheModule.register()],
   controllers: [StoreController],
   providers: [StoreService],
   exports: [StoreService, StoreModule],
--- src/store/store.service.ts
+++ src/store/store.service.ts
@@ -1,23 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Store } from './entities/store.entity';
 import { Repository } from 'typeorm';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Cache } from 'cache-manager';
 
 @Injectable()
 export class StoreService {
   constructor(
     @InjectRepository(Store)
     private storeRepositary: Repository<Store>,
+    @Inject(CACHE_MANAGER)
+    private cacheManager: Cache,
   ) {}
+
+  cacheActiveStoresKey = 'activeStores';
+  cacheActiveStoresTTL = 60000;
 
   async getStores(): Promise<Store[]> {
     return await this.storeRepositary.find({});
   }
 
-  async getActiveStores(): Promise<Store[]> {
-    return await this.storeRepositary.findBy({
+  async getActiveStores(): Promise<any> {
+    const cacheActiveStores = await this.cacheManager.get(
+      this.cacheActiveStoresKey,
+    );
+    if (cacheActiveStores) {
+      return cacheActiveStores;
+    }
+
+    const stores = await this.storeRepositary.findBy({
       isActive: true,
     });
+
+    await this.cacheManager.set(
+      this.cacheActiveStoresKey,
+      stores,
+      this.cacheActiveStoresTTL,
+    );
+
+    return stores;
   }
 
   async getStoresByCityId(cityId: number): Promise<Store[]> {
