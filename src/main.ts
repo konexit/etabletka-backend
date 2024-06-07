c77cff0cd5745bf7b35b58182df08d791e013394
Davyd Zaichenko <davyd67@gmail.com>
2024-06-07 16:27:55 +0300

    added config for run app

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,16 @@ import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const { APP_PORT, APP_HOST, ACCESS_CONTROL_ALLOW_ORIGIN } = process.env
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: ACCESS_CONTROL_ALLOW_ORIGIN,
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      preflightContinue: false,
+      optionsSuccessStatus: 204,
+    }
+  });
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen(APP_PORT, APP_HOST);
 }
 bootstrap();
