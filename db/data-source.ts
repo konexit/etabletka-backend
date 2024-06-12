da3a06ab31826614e4e449142f704fb351625780
Davyd Zaichenko <davyd67@gmail.com>
2024-06-12 16:47:03 +0300

    disabled auto sync entities

--- db/data-source.ts
+++ db/data-source.ts
@@ -9,7 +9,7 @@ export const dataSourceOptions: DataSourceOptions = {
   database: 'etabletka',
   entities: ['dist/**/*.entity.js'],
   migrations: ['dist/db/migrations/*.js'],
-  synchronize: true,
+  synchronize: false,
 };
 
 const dataSource = new DataSource(dataSourceOptions);
