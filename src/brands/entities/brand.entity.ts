40a4a1552cc69b3b00ef61faae50caeb103bb065
Davyd Zaichenko <davyd67@gmail.com>
2024-06-13 10:52:15 +0300

    add brand

---
+++ src/brands/entities/brand.entity.ts
@@ -0,0 +1,70 @@
+import {
+    Column,
+    Entity,
+    PrimaryGeneratedColumn,
+    CreateDateColumn,
+    UpdateDateColumn,
+} from 'typeorm';
+import { Exclude } from 'class-transformer';
+@Entity({
+    name: 'brands',
+})
+export class Brand {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ name: 'sync_id', nullable: true, unique: true })
+    @Exclude()
+    syncId: number;
+
+    @Column({ type: 'json', nullable: true })
+    name: JSON;
+
+    @Column({ nullable: true })
+    slug: string;
+
+    @Column({ nullable: true })
+    image: string;
+
+    @Column({ name: 'cdn_data', type: 'json', nullable: true })
+    @Exclude()
+    cdnData: JSON;
+
+    @Column({ default: 0 })
+    @Exclude()
+    position: number;
+
+    @Column({ default: true })
+    @Exclude()
+    active: boolean;
+
+    @Column({ default: true })
+    @Exclude()
+    popular: boolean;
+
+    @Column({ type: 'json', nullable: true })
+    description: JSON;
+
+    @Column({ name: 'seo_h1', type: 'json', nullable: true })
+    seoH1: JSON;
+
+    @Column({ name: 'seo_title', type: 'json', nullable: true })
+    seoTitle: JSON;
+
+    @Column({ name: 'seo_description', type: 'json', nullable: true })
+    seoDescription: JSON;
+
+    @Column({ name: 'seo_keywords', type: 'json', nullable: true })
+    seoKeywords: JSON;
+
+    @Column({ name: 'seo_text', type: 'json', nullable: true })
+    seoText: JSON;
+
+    @CreateDateColumn({ name: 'created_at', type: 'timestamp' })
+    @Exclude()
+    createdAt: Date;
+
+    @UpdateDateColumn({ name: 'updated_at', type: 'timestamp' })
+    @Exclude()
+    updatedAt: Date;
+}
