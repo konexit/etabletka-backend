36418853f1ef3154b3cfcfbbf154858e1a2384c4
Andrew <aks13@ukr.net>
2024-06-13 15:01:03 +0300

    Reverted

--- src/main.ts
+++ src/main.ts
@@ -3,30 +3,30 @@ import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 
-// async function bootstrap() {
-//   const { APP_PORT, APP_HOST, ACCESS_CONTROL_ALLOW_ORIGIN } = process.env;
-//   const app = await NestFactory.create(AppModule, {
-//     cors: {
-//       origin: ACCESS_CONTROL_ALLOW_ORIGIN,
-//       methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-//       preflightContinue: false,
-//       optionsSuccessStatus: 204,
-//     },
-//   });
-//
-//   app.useGlobalPipes(new ValidationPipe());
-//   await app.listen(APP_PORT, APP_HOST);
-// }
-// bootstrap();
-
-// TODO: Do not remove this code. I use it for docker
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const { APP_PORT, APP_HOST, ACCESS_CONTROL_ALLOW_ORIGIN } = process.env;
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: ACCESS_CONTROL_ALLOW_ORIGIN,
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      preflightContinue: false,
+      optionsSuccessStatus: 204,
+    },
+  });
+
   app.useGlobalPipes(new ValidationPipe());
-
-  const configService = app.get(ConfigService);
-  const port = configService.get<number>('APP_PORT');
-
-  await app.listen(port);
+  await app.listen(APP_PORT, APP_HOST);
 }
 bootstrap();
+
+// TODO: Do not remove this code. I use it for docker
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule, { cors: true });
+//   app.useGlobalPipes(new ValidationPipe());
+//
+//   const configService = app.get(ConfigService);
+//   const port = configService.get<number>('APP_PORT');
+//
+//   await app.listen(port);
+// }
+// bootstrap();
