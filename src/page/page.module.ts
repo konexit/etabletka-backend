e9b1a11abaf0bea608b6d737b8b5c77966426268
Andrew <aks13@ukr.net>
2024-06-18 12:44:56 +0300

    Cache pages

--- src/page/page.module.ts
+++ src/page/page.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Page } from './entities/page.entity';
 import { PageController } from './page.controller';
 import { PageService } from './page.service';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Page])],
+  imports: [TypeOrmModule.forFeature([Page]), CacheModule.register()],
   controllers: [PageController],
   providers: [PageService],
   exports: [PageService, PageModule],
--- src/page/page.service.ts
+++ src/page/page.service.ts
@@ -1,8 +1,10 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Inject, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Page } from './entities/page.entity';
 import { Repository } from 'typeorm';
 import { JwtService } from '@nestjs/jwt';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Cache } from 'cache-manager';
 
 @Injectable()
 export class PageService {
@@ -10,12 +12,23 @@ export class PageService {
     @InjectRepository(Page)
     private pageRepository: Repository<Page>,
     private jwtService: JwtService,
+    @Inject(CACHE_MANAGER)
+    private cacheManager: Cache,
   ) {}
+
+  cachePagesKey = 'pages';
+  cacheMenuPagesKey = 'menuPages';
+  cachePageTTL = 14400000; // 4 Hour
 
   async getPages(token: string | any[]): Promise<any> {
     // if (!token || typeof token !== 'string') {
     //   throw new HttpException('Forbidden', HttpStatus.FORBIDDEN);
     // }
+
+    const cachePages = await this.cacheManager.get(this.cachePagesKey);
+    if (cachePages) {
+      return cachePages;
+    }
 
     const pages = await this.pageRepository.find({
       where: { isPublished: true },
@@ -25,10 +38,17 @@ export class PageService {
       throw new HttpException('Pages not found', HttpStatus.NOT_FOUND);
     }
 
+    await this.cacheManager.set(this.cachePagesKey, pages, this.cachePageTTL);
+
     return pages;
   }
 
-  async getPagesByMenuIndex(index): Promise<Page[]> {
+  async getPagesByMenuIndex(index): Promise<any> {
+    const cachePages = await this.cacheManager.get(this.cacheMenuPagesKey);
+    if (cachePages) {
+      return cachePages;
+    }
+
     const queryBuilder = this.pageRepository.createQueryBuilder('page');
     const pages = await queryBuilder
       .select('page.id')
@@ -40,6 +60,12 @@ export class PageService {
     if (!pages) {
       throw new HttpException('Pages not found', HttpStatus.NOT_FOUND);
     }
+
+    await this.cacheManager.set(
+      this.cacheMenuPagesKey,
+      pages,
+      this.cachePageTTL,
+    );
 
     return pages;
   }
