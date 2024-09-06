7928b6c4c8e6463f0346694c17bb9b1b1d1b7bbc
Andrew <aks13@ukr.net>
2024-09-06 10:53:30 +0300

    Updated: Comment for product

--- src/productComment/dto/update-product-comment.dto.ts
+++ src/productComment/dto/update-product-comment.dto.ts
@@ -2,7 +2,7 @@ import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class UpdateProductComment {
   @IsNotEmpty()
-  postId: number;
+  productId: number;
 
   @IsNotEmpty()
   userId: number;
