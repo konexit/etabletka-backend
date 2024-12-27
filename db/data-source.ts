583a312d069c925e966d2db7a027390356e29297
Davyd Zaichenko <davyd67@gmail.com>
2024-12-27 15:55:26 +0200

    add load env db from .env

--- db/data-source.ts
+++ db/data-source.ts
@@ -1,12 +1,15 @@
 import { DataSource, DataSourceOptions } from 'typeorm';
+import 'dotenv/config';
+
+const { POSTGRES_HOST, POSTGRES_PORT, POSTGRES_NAME, POSTGRES_USER, POSTGRES_PASSWORD } = process.env;
 
 export const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
-  host: '10.10.1.15',
-  port: 5432,
-  username: 'etabletka',
-  password: 'qTK0vU4d$JA0rJTQosWD',
-  database: 'etabletka',
+  host: POSTGRES_HOST,
+  port: Number(POSTGRES_PORT),
+  username: POSTGRES_USER,
+  password: POSTGRES_PASSWORD,
+  database: POSTGRES_NAME,
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
   migrations: ['dist/db/migrations/*.js'],
   synchronize: false
