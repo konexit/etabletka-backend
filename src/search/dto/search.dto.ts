587f77e161b4cf7f128790f95d1a625e0b3de134
Denys <denostapmen@gmail.com>
2025-04-25 15:49:36 +0300

    fix(search): add type annotations to SearchDto

--- src/search/dto/search.dto.ts
+++ src/search/dto/search.dto.ts
@@ -1,12 +1,28 @@
-import { IsString, Length } from 'class-validator';
+import { IsArray, IsNumber, IsOptional, IsString, Length } from 'class-validator';
 
 export class SearchDto {
   @Length(0, 255)
   @IsString()
   text: string;
+
+  @IsOptional()
+  @IsString()
   lang?: string;
+
+  @IsOptional()
+  @IsNumber()
   limit?: number;
+
+  @IsOptional()
+  @IsNumber()
   offset?: number;
+
+  @IsOptional()
+  @IsString()
   filter?: string;
+
+  @IsOptional()
+  @IsArray()
+  @IsString({ each: true })
   sort?: string[];
 }
